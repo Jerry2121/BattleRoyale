e237268d95b08e7e37477b0e215aa1d8ee379819
Jeremiah Templeton <jerry2121121021@yahoo.com>
2018-11-01 09:59:24 -0700

    Optimization -JT

---
+++ BattleRoyale/Assets/!JT/Pause.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pause : MonoBehaviour {
+    public bool paused;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.Escape) && !paused)
+        {
+            paused = true;
+            Time.timeScale = 0;
+        }
+        else if (Input.GetKeyDown(KeyCode.Escape) && paused)
+        {
+            paused = false;
+            Time.timeScale = 1;
+        }
+	}
+}
