0c20eee0af28b14e0557c5e283e6d08ad4f9dbd2
SwordOfWrynn <bubba.wiens@gmail.com>
2018-11-06 10:31:24 -0800

    Foo

---
+++ BattleRoyale/Assets/!AW/Scripts/WeaponGraphics.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponGraphics : MonoBehaviour {
+
+    public ParticleSystem muzzleFlash;
+    public GameObject impactEffectPrefab;
+
+}
---
+++ BattleRoyale/Assets/!AW/Scripts/WeaponManager.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class WeaponManager : NetworkBehaviour {
+
+    [SerializeField]
+    string weaponLayerName = "Weapon";
+    [SerializeField]
+    PlayerWeapon primaryWeapon;
+    [SerializeField]
+    Transform weaponHolder;
+
+    private PlayerWeapon currentWeapon;
+    private WeaponGraphics currentGraphics;
+
+	// Use this for initialization
+	void Start () {
+        EquipWeapon(primaryWeapon);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void EquipWeapon(PlayerWeapon _weapon)
+    {
+        currentWeapon = _weapon;
+
+        GameObject weaponIns = Instantiate(_weapon.graphics, weaponHolder.position, weaponHolder.rotation);
+        weaponIns.transform.SetParent(weaponHolder);
+        currentGraphics = weaponIns.GetComponent<WeaponGraphics>();
+        if(currentGraphics == null)
+        {
+            Debug.LogError("WeaponManager -- EquipWeapon: There is no WeaponGraphics on the " + weaponIns.name +" weapon object!");
+        }
+
+        if (isLocalPlayer)
+        {
+            weaponIns.layer = LayerMask.NameToLayer(weaponLayerName);
+        }
+
+    }
+
+    public PlayerWeapon GetCurrentWeapon()
+    {
+        return currentWeapon;
+    }
+
+    public WeaponGraphics GetWeaponGraphics()
+    {
+        return currentGraphics;
+    }
+
+}
