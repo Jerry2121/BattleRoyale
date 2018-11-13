a3a9cd74d9b5c84dc831e8550d895811824db8e9
SwordOfWrynn <bubba.wiens@gmail.com>
2018-11-13 08:53:50 -0800

    Foo

---
+++ BattleRoyale/Assets/!AW/Scripts/NetworkDiscoveryScript.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class NetworkDiscoveryScript : NetworkDiscovery {
+
+    private NetworkManager networkManager;
+    private NetworkDiscovery networkDiscovery;
+
+	// Use this for initialization
+	void Start () {
+        networkManager = GetComponent<NetworkManager>();
+        Initialize();
+    }
+
+    public void JoinLANGame()
+    {
+        StartAsClient();
+    }
+
+    public override void OnReceivedBroadcast(string fromAddress, string data)
+    {
+        networkManager.networkAddress = fromAddress;
+        //Keep it from trying to start and make a new player multipule times
+        if (networkManager.IsClientConnected() == false)
+            networkManager.StartClient();
+    }
+
+    public void CreateLANGameAsHost()
+    {
+        networkManager.StartHost();
+        StartAsServer();
+    }
+    public void CreateLANGameAsServer()
+    {
+        networkManager.StartServer();
+        StartAsServer();
+    }
+
+}
