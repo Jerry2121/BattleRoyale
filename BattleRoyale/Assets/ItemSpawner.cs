fc456553f97ecf5084d263837ccbcd648569b138
SwordOfWrynn <bubba.wiens@gmail.com>
2018-12-07 08:49:03 -0800

    Made a scripts folder in Assets, and moved most of my scripts there

--- BattleRoyale/Assets/ItemSpawner.cs
+++ BattleRoyale/Assets/ItemSpawner.cs
@@ -1,14 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking;
 
 public class ItemSpawner : MonoBehaviour {
     [Header("Spawner Type")]
     public bool WeaponSpawner;
     public bool AmmoSpawner;
     [Header("AmmoSpawners")]
-    public GameObject AmmoSpawwner1;
-    public GameObject AmmoSpawwner2;
+    public GameObject AmmoSpawner1;
+    public GameObject AmmoSpawner2;
     [Header("Weapon Bools")]
     public bool canspawnItem;
     public bool ran;
@@ -19,89 +20,114 @@ public class ItemSpawner : MonoBehaviour {
     public bool Weapon5;
     public bool Weapon6;
     private int d1;
-	// Use this for initialization
-	void Start () {
+
+    NetworkManager networkManager;
+    NetworkDiscoveryScript networkDiscoveryScript;
+
+    // Use this for initialization
+    void Start () {
         canspawnItem = true;
         ran = false;
-	}
+
+        networkManager = NetworkManager.singleton;
+        networkDiscoveryScript = networkManager.GetComponent<NetworkDiscoveryScript>();
+    }
 	
 	// Update is called once per frame
 	void Update () {
-        if (canspawnItem)
+        if (canspawnItem && networkDiscoveryScript.isServer)
         {
             d1 = Random.Range(0, 7);
-            if (d1 == 1 && GameObject.Find("GameManager").GetComponent<GameManagerScript>().DisableItemSpawning == false && WeaponSpawner && canspawnItem && !ran)
+            if (d1 == 1 && GameObject.Find("_GameManager").GetComponent<GameManagerScript>().DisableItemSpawning == false && WeaponSpawner && canspawnItem && !ran)
             {
                 Weapon1 = true;
-                Instantiate(GameObject.Find("GameManager").GetComponent<GameManagerScript>().Weapon1, new Vector3(transform.position.x, transform.position.y, transform.position.z), Quaternion.identity);
+                GameObject GO = Instantiate(GameObject.Find("_GameManager").GetComponent<GameManagerScript>().Weapon1, new Vector3(transform.position.x, transform.position.y, transform.position.z), Quaternion.identity);
                 ran = true;
+                NetworkServer.Spawn(GO);
             }
-            if (d1 == 2 && GameObject.Find("GameManager").GetComponent<GameManagerScript>().DisableItemSpawning == false && WeaponSpawner && canspawnItem && !ran)
+            if (d1 == 2 && GameObject.Find("_GameManager").GetComponent<GameManagerScript>().DisableItemSpawning == false && WeaponSpawner && canspawnItem && !ran)
             {
                 Weapon2 = true;
-                Instantiate(GameObject.Find("GameManager").GetComponent<GameManagerScript>().Weapon2, new Vector3(transform.position.x, transform.position.y, transform.position.z), Quaternion.identity);
+                GameObject GO = Instantiate(GameObject.Find("_GameManager").GetComponent<GameManagerScript>().Weapon2, new Vector3(transform.position.x, transform.position.y, transform.position.z), Quaternion.identity);
                 ran = true;
+                NetworkServer.Spawn(GO);
             }
-            if (d1 == 3 && GameObject.Find("GameManager").GetComponent<GameManagerScript>().DisableItemSpawning == false && WeaponSpawner && canspawnItem && !ran)
+            if (d1 == 3 && GameObject.Find("_GameManager").GetComponent<GameManagerScript>().DisableItemSpawning == false && WeaponSpawner && canspawnItem && !ran)
             {
                 Weapon3 = true;
-                Instantiate(GameObject.Find("GameManager").GetComponent<GameManagerScript>().Weapon3, new Vector3(transform.position.x, transform.position.y, transform.position.z), Quaternion.identity);
+                GameObject GO = Instantiate(GameObject.Find("_GameManager").GetComponent<GameManagerScript>().Weapon3, new Vector3(transform.position.x, transform.position.y, transform.position.z), Quaternion.identity);
                 ran = true;
+                NetworkServer.Spawn(GO);
             }
-            if (d1 == 4 && GameObject.Find("GameManager").GetComponent<GameManagerScript>().DisableItemSpawning == false && WeaponSpawner && canspawnItem && !ran)
+            if (d1 == 4 && GameObject.Find("_GameManager").GetComponent<GameManagerScript>().DisableItemSpawning == false && WeaponSpawner && canspawnItem && !ran)
             {
                 Weapon4 = true;
-                Instantiate(GameObject.Find("GameManager").GetComponent<GameManagerScript>().Weapon4, new Vector3(transform.position.x, transform.position.y, transform.position.z), Quaternion.identity);
+                GameObject GO = Instantiate(GameObject.Find("_GameManager").GetComponent<GameManagerScript>().Weapon4, new Vector3(transform.position.x, transform.position.y, transform.position.z), Quaternion.identity);
                 ran = true;
+                NetworkServer.Spawn(GO);
             }
-            if (d1 == 5 && GameObject.Find("GameManager").GetComponent<GameManagerScript>().DisableItemSpawning == false && WeaponSpawner && canspawnItem && !ran)
+            if (d1 == 5 && GameObject.Find("_GameManager").GetComponent<GameManagerScript>().DisableItemSpawning == false && WeaponSpawner && canspawnItem && !ran)
             {
                 Weapon5 = true;
-                Instantiate(GameObject.Find("GameManager").GetComponent<GameManagerScript>().Weapon5, new Vector3(transform.position.x, transform.position.y, transform.position.z), Quaternion.identity);
+                GameObject GO = Instantiate(GameObject.Find("_GameManager").GetComponent<GameManagerScript>().Weapon5, new Vector3(transform.position.x, transform.position.y, transform.position.z), Quaternion.identity);
                 ran = true;
+                NetworkServer.Spawn(GO);
             }
-            if (d1 == 6 && GameObject.Find("GameManager").GetComponent<GameManagerScript>().DisableItemSpawning == false && WeaponSpawner && canspawnItem && !ran)
+            if (d1 == 6 && GameObject.Find("_GameManager").GetComponent<GameManagerScript>().DisableItemSpawning == false && WeaponSpawner && canspawnItem && !ran)
             {
                 Weapon6 = true;
-                Instantiate(GameObject.Find("GameManager").GetComponent<GameManagerScript>().Weapon6, new Vector3(transform.position.x, transform.position.y, transform.position.z), Quaternion.identity);
+                GameObject GO = Instantiate(GameObject.Find("_GameManager").GetComponent<GameManagerScript>().Weapon6, new Vector3(transform.position.x, transform.position.y, transform.position.z), Quaternion.identity);
                 ran = true;
+                NetworkServer.Spawn(GO);
             }
             //Ammo
-            if (Weapon1 && GameObject.Find("GameManager").GetComponent<GameManagerScript>().DisableItemSpawning == false && ran)
+            if (Weapon1 && GameObject.Find("_GameManager").GetComponent<GameManagerScript>().DisableItemSpawning == false && ran)
             {
-                Instantiate(GameObject.Find("GameManager").GetComponent<GameManagerScript>().Ammo1, new Vector3(AmmoSpawwner1.transform.position.x, AmmoSpawwner1.transform.position.y, AmmoSpawwner1.transform.position.z), Quaternion.identity);
-                Instantiate(GameObject.Find("GameManager").GetComponent<GameManagerScript>().Ammo1, new Vector3(AmmoSpawwner2.transform.position.x, AmmoSpawwner2.transform.position.y, AmmoSpawwner2.transform.position.z), Quaternion.identity);
+                GameObject GO = Instantiate(GameObject.Find("_GameManager").GetComponent<GameManagerScript>().Ammo1, new Vector3(AmmoSpawner1.transform.position.x, AmmoSpawner1.transform.position.y, AmmoSpawner1.transform.position.z), Quaternion.identity);
+                GameObject GO2 = Instantiate(GameObject.Find("_GameManager").GetComponent<GameManagerScript>().Ammo1, new Vector3(AmmoSpawner2.transform.position.x, AmmoSpawner2.transform.position.y, AmmoSpawner2.transform.position.z), Quaternion.identity);
                 canspawnItem = false;
+                NetworkServer.Spawn(GO);
+                NetworkServer.Spawn(GO2);
             }
-            if (Weapon2 && GameObject.Find("GameManager").GetComponent<GameManagerScript>().DisableItemSpawning == false && ran)
+            if (Weapon2 && GameObject.Find("_GameManager").GetComponent<GameManagerScript>().DisableItemSpawning == false && ran)
             {
-                Instantiate(GameObject.Find("GameManager").GetComponent<GameManagerScript>().Ammo2, new Vector3(AmmoSpawwner1.transform.position.x, AmmoSpawwner1.transform.position.y, AmmoSpawwner1.transform.position.z), Quaternion.identity);
-                Instantiate(GameObject.Find("GameManager").GetComponent<GameManagerScript>().Ammo2, new Vector3(AmmoSpawwner2.transform.position.x, AmmoSpawwner2.transform.position.y, AmmoSpawwner2.transform.position.z), Quaternion.identity);
+                GameObject GO = Instantiate(GameObject.Find("_GameManager").GetComponent<GameManagerScript>().Ammo2, new Vector3(AmmoSpawner1.transform.position.x, AmmoSpawner1.transform.position.y, AmmoSpawner1.transform.position.z), Quaternion.identity);
+                GameObject GO2 = Instantiate(GameObject.Find("_GameManager").GetComponent<GameManagerScript>().Ammo2, new Vector3(AmmoSpawner2.transform.position.x, AmmoSpawner2.transform.position.y, AmmoSpawner2.transform.position.z), Quaternion.identity);
                 canspawnItem = false;
+                NetworkServer.Spawn(GO);
+                NetworkServer.Spawn(GO2);
             }
-            if (Weapon3 && GameObject.Find("GameManager").GetComponent<GameManagerScript>().DisableItemSpawning == false && ran)
+            if (Weapon3 && GameObject.Find("_GameManager").GetComponent<GameManagerScript>().DisableItemSpawning == false && ran)
             {
-                Instantiate(GameObject.Find("GameManager").GetComponent<GameManagerScript>().Ammo1, new Vector3(AmmoSpawwner1.transform.position.x, AmmoSpawwner1.transform.position.y, AmmoSpawwner1.transform.position.z), Quaternion.identity);
-                Instantiate(GameObject.Find("GameManager").GetComponent<GameManagerScript>().Ammo1, new Vector3(AmmoSpawwner2.transform.position.x, AmmoSpawwner2.transform.position.y, AmmoSpawwner2.transform.position.z), Quaternion.identity);
+                GameObject GO = Instantiate(GameObject.Find("_GameManager").GetComponent<GameManagerScript>().Ammo1, new Vector3(AmmoSpawner1.transform.position.x, AmmoSpawner1.transform.position.y, AmmoSpawner1.transform.position.z), Quaternion.identity);
+                GameObject GO2 = Instantiate(GameObject.Find("_GameManager").GetComponent<GameManagerScript>().Ammo1, new Vector3(AmmoSpawner2.transform.position.x, AmmoSpawner2.transform.position.y, AmmoSpawner2.transform.position.z), Quaternion.identity);
                 canspawnItem = false;
+                NetworkServer.Spawn(GO);
+                NetworkServer.Spawn(GO2);
             }
-            if (Weapon4 && GameObject.Find("GameManager").GetComponent<GameManagerScript>().DisableItemSpawning == false && ran)
+            if (Weapon4 && GameObject.Find("_GameManager").GetComponent<GameManagerScript>().DisableItemSpawning == false && ran)
             {
-                Instantiate(GameObject.Find("GameManager").GetComponent<GameManagerScript>().Ammo3, new Vector3(AmmoSpawwner1.transform.position.x, AmmoSpawwner1.transform.position.y, AmmoSpawwner1.transform.position.z), Quaternion.identity);
-                Instantiate(GameObject.Find("GameManager").GetComponent<GameManagerScript>().Ammo3, new Vector3(AmmoSpawwner2.transform.position.x, AmmoSpawwner2.transform.position.y, AmmoSpawwner2.transform.position.z), Quaternion.identity);
+                GameObject GO = Instantiate(GameObject.Find("_GameManager").GetComponent<GameManagerScript>().Ammo3, new Vector3(AmmoSpawner1.transform.position.x, AmmoSpawner1.transform.position.y, AmmoSpawner1.transform.position.z), Quaternion.identity);
+                GameObject GO2 = Instantiate(GameObject.Find("_GameManager").GetComponent<GameManagerScript>().Ammo3, new Vector3(AmmoSpawner2.transform.position.x, AmmoSpawner2.transform.position.y, AmmoSpawner2.transform.position.z), Quaternion.identity);
                 canspawnItem = false;
+                NetworkServer.Spawn(GO);
+                NetworkServer.Spawn(GO2);
             }
-            if (Weapon5 && GameObject.Find("GameManager").GetComponent<GameManagerScript>().DisableItemSpawning == false && ran)
+            if (Weapon5 && GameObject.Find("_GameManager").GetComponent<GameManagerScript>().DisableItemSpawning == false && ran)
             {
-                Instantiate(GameObject.Find("GameManager").GetComponent<GameManagerScript>().Ammo2, new Vector3(AmmoSpawwner1.transform.position.x, AmmoSpawwner1.transform.position.y, AmmoSpawwner1.transform.position.z), Quaternion.identity);
-                Instantiate(GameObject.Find("GameManager").GetComponent<GameManagerScript>().Ammo2, new Vector3(AmmoSpawwner2.transform.position.x, AmmoSpawwner2.transform.position.y, AmmoSpawwner2.transform.position.z), Quaternion.identity);
+                GameObject GO = Instantiate(GameObject.Find("_GameManager").GetComponent<GameManagerScript>().Ammo2, new Vector3(AmmoSpawner1.transform.position.x, AmmoSpawner1.transform.position.y, AmmoSpawner1.transform.position.z), Quaternion.identity);
+                GameObject GO2 = Instantiate(GameObject.Find("_GameManager").GetComponent<GameManagerScript>().Ammo2, new Vector3(AmmoSpawner2.transform.position.x, AmmoSpawner2.transform.position.y, AmmoSpawner2.transform.position.z), Quaternion.identity);
                 canspawnItem = false;
+                NetworkServer.Spawn(GO);
+                NetworkServer.Spawn(GO2);
             }
-            if (Weapon6 && GameObject.Find("GameManager").GetComponent<GameManagerScript>().DisableItemSpawning == false && ran)
+            if (Weapon6 && GameObject.Find("_GameManager").GetComponent<GameManagerScript>().DisableItemSpawning == false && ran)
             {
-                Instantiate(GameObject.Find("GameManager").GetComponent<GameManagerScript>().Ammo1, new Vector3(AmmoSpawwner1.transform.position.x, AmmoSpawwner1.transform.position.y, AmmoSpawwner1.transform.position.z), Quaternion.identity);
-                Instantiate(GameObject.Find("GameManager").GetComponent<GameManagerScript>().Ammo1, new Vector3(AmmoSpawwner2.transform.position.x, AmmoSpawwner2.transform.position.y, AmmoSpawwner2.transform.position.z), Quaternion.identity);
+                GameObject GO = Instantiate(GameObject.Find("_GameManager").GetComponent<GameManagerScript>().Ammo1, new Vector3(AmmoSpawner1.transform.position.x, AmmoSpawner1.transform.position.y, AmmoSpawner1.transform.position.z), Quaternion.identity);
+                GameObject GO2 = Instantiate(GameObject.Find("_GameManager").GetComponent<GameManagerScript>().Ammo1, new Vector3(AmmoSpawner2.transform.position.x, AmmoSpawner2.transform.position.y, AmmoSpawner2.transform.position.z), Quaternion.identity);
                 canspawnItem = false;
+                NetworkServer.Spawn(GO);
+                NetworkServer.Spawn(GO2);
             }
         }
 	}
