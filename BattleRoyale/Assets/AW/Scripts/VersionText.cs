e9da58dc69e1a011308b7e43634fd1d7d8c4ec29
SwordOfWrynn <bubba.wiens@gmail.com>
2018-12-12 08:20:52 -0800

    Updated the version text

---
+++ BattleRoyale/Assets/AW/Scripts/VersionText.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class VersionText : MonoBehaviour {
+
+    TextMeshProUGUI versionText;
+
+	// Use this for initialization
+	void Start () {
+        versionText = GetComponent<TextMeshProUGUI>();
+        if(versionText != null)
+            versionText.text = Application.version;
+	}
+}
