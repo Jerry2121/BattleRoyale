51dc38824f9b33d8cd0df314ce7e24eda147a77f
Jeremiah Templeton <jerry2121121021@yahoo.com>
2018-12-13 09:08:07 -0800

    UI Fixes

---
+++ BattleRoyale/Assets/AJT/AirDropScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AirDropScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
