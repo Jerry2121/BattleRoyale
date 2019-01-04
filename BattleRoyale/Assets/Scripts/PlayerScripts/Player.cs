a8946b23890b8aca4606ad98078a689f45b860d5
Jeremiah Templeton <jerry2121121021@yahoo.com>
2019-01-04 10:17:42 -0800

    Blah

--- BattleRoyale/Assets/Scripts/PlayerScripts/Player.cs
+++ BattleRoyale/Assets/Scripts/PlayerScripts/Player.cs
@@ -328,15 +328,15 @@ public class Player : NetworkBehaviour {
             if (outsideOfZoneImage != null)
                 outsideOfZoneImage.SetActive(false);
         }
-    }
-    private void OnTriggerStay(Collider other)
-    {
         Utility.WaitForEndOfFrame();
         if (other.gameObject.tag == "Lobby" && GameManager.instance.inStartPeriod == false)
         {
-            if(isLocalPlayer)
+            if (isLocalPlayer)
                 CmdTakeDamage(999999999, "Dev");
         }
+    }
+    private void OnTriggerStay(Collider other)
+    {
     }
 
 }
