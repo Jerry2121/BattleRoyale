fc21511a918a0d1a78b538f00c2b03f390e2d9da
Nathan Weis <DrakonRider598@yahoo.com>
2019-01-28 10:28:14 -0800

    terrible particale effect changes

---
+++ BattleRoyale/Assets/ANW/Particle Prefabs/ParticleMapVisuals.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleMapVisuals : MonoBehaviour {
+
+    public float height;
+
+    void Start()
+    {
+        //GetComponent<Transform>().position = new Vector3(0, height, 0);
+    }
+}
