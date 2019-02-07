ed134929c8ecd46958e310f9921dca68b02deb75
SwordOfWrynn <bubba.wiens@gmail.com>
2019-02-07 09:13:30 -0800

    Foo

--- BattleRoyale/Assets/Scripts/LobbyScripts/Credits.cs
+++ BattleRoyale/Assets/Scripts/LobbyScripts/Credits.cs
@@ -4,9 +4,16 @@ using UnityEngine;
 
 public class Credits : MonoBehaviour {
 
+    public float speed = 1f;
+
+    public GameObject creditsCanvas;
+    public Transform credits;
+
+    private Vector3 startingPos;
+
 	// Use this for initialization
 	void Start () {
-		
+        startingPos = credits.position;
 	}
 	
 	// Update is called once per frame
